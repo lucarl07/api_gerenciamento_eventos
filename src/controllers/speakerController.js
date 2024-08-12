1d6c7be521631a8008f3a642c6efee6291ab49ea
lucarl07 <junitinho2007@gmail.com>
2024-08-12 09:17:39 -0300

    🕹️ controller: New endpoint GET /eventos/participantes

--- src/controllers/speakerController.js
+++ src/controllers/speakerController.js
@@ -3,7 +3,21 @@ import conn from "../config/conn.js";
 import { v4 as uuidv4 } from "uuid";
 
 // Adicionar um novo palestrante:
-export const addSpeaker = (request, response) => {}
+export const addSpeaker = (req, res) => {}
 
 // Pesquisar por todos os palestrantes:
-export const getAllSpeakers = (request, response) => {}
+export const getAllSpeakers = (req, res) => {
+  const searchSQL = /*sql*/ `SELECT * FROM palestrantes`
+
+  conn.query(searchSQL, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        err: "Erro interno ao buscar os palestrantes. Tente novamente mais tarde."
+      });
+    }
+
+    const speakers = data;
+    res.status(200).json(speakers)
+  })
+}
