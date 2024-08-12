b0a88f8c629dc203c55f943380152a4bce3243b7
lucarl07 <junitinho2007@gmail.com>
2024-08-12 09:27:31 -0300

    🧩 middleware: Add validation for new speakers

--- src/routes/speakerRoutes.js
+++ src/routes/speakerRoutes.js
@@ -2,17 +2,19 @@
 import { Router } from "express";
 
 // Módulos de controle:
-import { 
-  addSpeaker, getAllSpeakers 
-} from "../controllers/speakerController.js"
+import {
+  addSpeaker,
+  getAllSpeakers,
+} from "../controllers/speakerController.js";
 
 // Módulos auxiliares (Middleware):
+import validateNewSpeaker from "../middleware/validateNewSpeaker.js";
 
 // Inicializando o roteador:
 const router = Router();
 
 // Endpoints:
-router.post("/", addSpeaker);
+router.post("/", validateNewSpeaker, addSpeaker);
 router.get("/", getAllSpeakers);
 
 export default router;
