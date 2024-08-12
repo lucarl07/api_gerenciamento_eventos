8beb06025b9f6d826a491b95c42cbf709de2d038
lucarl07 <junitinho2007@gmail.com>
2024-08-12 08:21:43 -0300

    :construction: build: Initial server.js settings

---
+++ src/server.js
@@ -0,0 +1,42 @@
+/** CONFERENCE+ API - Por: Luiz Carlos dos Santos Júnior 
+ * 
+ * Aplicação desenvolvida com Express.js para a avaliação 
+ * de Interação com APIs, que ocorrerá dos dias 12/08/2024 
+ * à 16/08/2024.
+ * 
+ **/
+
+// Dependências:
+import "dotenv/config";
+import express from "express";
+import path from "node:path";
+
+// Importação de modelos de dados (entidades):
+
+// Importação de roteadores:
+
+// Gerando constantes do servidor:
+const PORT = process.env.PORT
+const app = express();
+
+// Middlewares globais:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Ao iniciar o servidor:
+app.listen(PORT, () => {
+  console.clear();
+  console.log("=:=:=:= BEM VINDO À CONFERENCE+ API! =:=:=:=")
+  console.log("Sua melhor aliada na organização de eventos!")
+  console.log(`Servidor no PORT: ${PORT} 🚀 \nVersão: 1.0.0 🔧 \n`)
+})
+
+// Rotas utilizadas:
+
+// Rota não encontrada:
+app.use((request, response) => {
+  response.status(404).json({
+    message: "Rota não encontrada.",
+    tip: "Certifique-se de que você digitou a URL corretamente."
+  })
+})
