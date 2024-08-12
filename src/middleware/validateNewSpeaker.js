e1a039c05ae2a906557f8d08e4b26ea1e4e4739b
lucarl07 <junitinho2007@gmail.com>
2024-08-12 09:42:45 -0300

    🐛 fix: No returns at validate new speaker middleware

--- src/middleware/validateNewSpeaker.js
+++ src/middleware/validateNewSpeaker.js
@@ -2,32 +2,32 @@ const validateNewSpeaker = (req, res, next) => {
   const { nome, expertise, dt_nascimento, cpf, email, senha } = req.body;
 
   if (!nome) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "O nome é obrigatório."
     })
   }
   if (!expertise) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "A especialização é obrigatória."
     })
   }
   if (!dt_nascimento) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "A data de nascimento é obrigatória."
     })
   }
   if (!cpf) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "O Cadastro de Pessoa Física (CPF) é obrigatório."
     })
   }
   if (!email) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "O e-mail é obrigatório."
     })
   }
   if (!senha) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "A senha é obrigatória."
     })
   }
