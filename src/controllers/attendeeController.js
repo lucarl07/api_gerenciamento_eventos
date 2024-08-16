c8af08d9c4b3803d01794cbd48051c8d61a0e66d
lucarl07 <junitinho2007@gmail.com>
2024-08-16 10:01:08 -0300

    🚧 controller: Add the attendee con. exports to its router

---
+++ src/controllers/attendeeController.js
@@ -0,0 +1,6 @@
+// Dependências / Conexões:
+import bcrypt from "bcrypt"
+import conn from "../config/conn.js";
+import { v4 as uuidv4 } from "uuid";
+
+export const addAttendee = (req, res) => {}
--- src/routes/attendeeRoutes.js
+++ src/routes/attendeeRoutes.js
@@ -1,10 +1,13 @@
 // Dependências:
 import { Router } from "express";
 
+// Módulos de controle:
+import { addAttendee } from "../controllers/attendeeController.js";
+
 // Inicializando o roteador:
 const router = Router();
 
 // Endpoints:
-router.post("/registrar")
+router.post("/registrar", addAttendee)
 
 export default router;
