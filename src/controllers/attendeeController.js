a3990427dbb9b548b4f2bd2d1f56fd89e676a2d2
lucarl07 <junitinho2007@gmail.com>
2024-08-16 10:16:53 -0300

    🕹️ controller: New endpoint POST /eventos/participantes/registrar

--- src/controllers/attendeeController.js
+++ src/controllers/attendeeController.js
@@ -3,4 +3,55 @@ import bcrypt from "bcrypt"
 import conn from "../config/conn.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const addAttendee = (req, res) => {}
+// Adicionar um novo participante:
+export const addAttendee = (req, res) => {
+  const { nome, dt_nascimento, email, senha } = req.body;
+
+  const checkUniqueSQL = {
+    s: /*sql*/ `SELECT * FROM palestrantes WHERE ?? = ?`,
+    v: ["email", email]
+  }
+
+  conn.query(checkUniqueSQL['s'], checkUniqueSQL['v'], async (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        err: "Erro interno ao buscar os participantes.",
+        tip: "Tente novamente mais tarde."
+      });
+    }
+
+    if (data.length > 0) {
+      return res.status(409).json({
+        message: "O e-mail inserido já está em uso."
+      })
+    }
+
+    // Tratamento de dados:
+    const id = uuidv4();
+    const salt = await bcrypt.genSalt(15)
+    const senhaHash = await bcrypt.hash(senha, salt)
+
+    const insertSQL = {
+      s: /*sql*/ `INSERT INTO participantes (??) VALUES (?)`,
+      v: [
+        ["participante_id", "nome", "dt_nascimento", "email", "senha"],
+        [id, nome, dt_nascimento, email, senhaHash]
+      ]
+    }
+
+    conn.query(insertSQL['s'], insertSQL['v'], (_err, _data) => {
+      if (_err) {
+        console.error(_err);
+        return res.status(500).json({
+          err: "Erro interno ao adicionar o participante.",
+          tip: "Tente novamente mais tarde."
+        });
+      }
+
+      res.status(201).json({
+        message: "Participante adicionado com sucesso!"
+      })
+    })
+  })
+}
